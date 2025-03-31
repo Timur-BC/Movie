7fd07eb622ce60824907c3e411e8b53aea73d0ad
Timur-BC <63474058+Timur-BC@users.noreply.github.com>
2025-03-31 05:35:41 +0500

    Update Header.jsx
    стр

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
           <nav className="nav">
             <ul className="nav-list">
               <li className="nav-item">
-                <Link to="/" className="nav-link">Главная стр</Link>
+                <Link to="/" className="nav-link">Главная</Link>
               </li>
               <li className="nav-item">
                 <Link to="/favorites" className="nav-link">
